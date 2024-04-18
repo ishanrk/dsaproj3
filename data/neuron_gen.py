5b7156730286caa386d5f4ae2797bbabff160468
mauricio.b.mossi <mauricio.b.mossi@gmail.com>
2024-04-18 17:50:31 -0400

    Benchmark report and generator. Tests for worst case scenerio as time complexity of generator function depends on number of adjacent nodes. To test, adjacent nodes were set to a specified value N per each node.

--- data/neuron_gen.py
+++ data/neuron_gen.py
@@ -12,12 +12,7 @@ field_to_index["connections"] = 4
 
 
 # Base W/ list
-def genNeuronsV1(num_of_neurons : int, num_of_regions : int = 8, max_connection_bias : float = 1.0, max_error : float = 0.2, max_adjacent : int = 50):
-
-    """Going to build multiple arrays, each will be matched according to index. Therefore, to get
-    data for say node 10, youll just need to do regions[10], connectionBias[10]... etc. We could extract
-    this into a wrapper, which will just abstract this and return an object.
-    """
+def genNeuronsV1(num_of_neurons : int, num_of_regions : int = 8, max_connection_bias : float = 1.0, max_error : float = 0.2, max_adjacent : int = 500, bench_mark : bool = False):
 
     regions = np.random.randint(0, num_of_regions, num_of_neurons)
     connection_bias = np.random.rand(num_of_neurons) * max_connection_bias
@@ -27,17 +22,12 @@ def genNeuronsV1(num_of_neurons : int, num_of_regions : int = 8, max_connection_
     choices = np.arange(num_of_neurons)
 
     for i in range(num_of_neurons):
-        connections[i] = np.random.choice(choices, np.random.randint(max_adjacent))
+        connections[i] = np.random.choice(choices, max_adjacent if bench_mark else np.random.randint(max_adjacent))
 
     return np.array([regions, connection_bias, error_bias, connections])
 
 # W/ tuple
-def genNeuronsV2(num_of_neurons : int, num_of_regions : int = 8, max_connection_bias : float = 1.0, max_error : float = 0.2, max_adjacent : int = 50):
-
-    """Going to build multiple arrays, each will be matched according to index. Therefore, to get
-    data for say node 10, youll just need to do regions[10], connectionBias[10]... etc. We could extract
-    this into a wrapper, which will just abstract this and return an object.
-    """
+def genNeuronsV2(num_of_neurons : int, num_of_regions : int = 8, max_connection_bias : float = 1.0, max_error : float = 0.2, max_adjacent : int = 500, bench_mark : bool = False):
 
     regions = np.random.randint(0, num_of_regions, num_of_neurons)
     connection_bias = np.random.rand(num_of_neurons) * max_connection_bias
@@ -47,17 +37,12 @@ def genNeuronsV2(num_of_neurons : int, num_of_regions : int = 8, max_connection_
     choices = np.arange(num_of_neurons)
 
     for i in range(num_of_neurons):
-        connections[i] = np.random.choice(choices, np.random.randint(max_adjacent))
+        connections[i] = np.random.choice(choices, max_adjacent if bench_mark else np.random.randint(max_adjacent))
 
     return np.array((regions, connection_bias, error_bias, connections))
 
 # W/ empty
-def genNeuronsV3(num_of_neurons : int, num_of_regions : int = 8, max_connection_bias : float = 1.0, max_error : float = 0.2, max_adjacent : int = 50):
-
-    """Going to build multiple arrays, each will be matched according to index. Therefore, to get
-    data for say node 10, youll just need to do regions[10], connectionBias[10]... etc. We could extract
-    this into a wrapper, which will just abstract this and return an object.
-    """
+def genNeuronsV3(num_of_neurons : int, num_of_regions : int = 8, max_connection_bias : float = 1.0, max_error : float = 0.2, max_adjacent : int = 500, bench_mark : bool = False):
 
     regions = np.random.randint(0, num_of_regions, num_of_neurons)
     connection_bias = np.random.rand(num_of_neurons) * max_connection_bias
@@ -67,7 +52,7 @@ def genNeuronsV3(num_of_neurons : int, num_of_regions : int = 8, max_connection_
     choices = np.arange(num_of_neurons)
 
     for i in range(num_of_neurons):
-        connections[i] = np.random.choice(choices, np.random.randint(max_adjacent))
+        connections[i] = np.random.choice(choices, max_adjacent if bench_mark else np.random.randint(max_adjacent))
 
     ret = np.empty(4, dtype=object)
 
