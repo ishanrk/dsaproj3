a8e2bc05ae7aa2c2d3a2e36f48a1de5b8dd50d1e
ishanrk <109903325+ishanrk@users.noreply.github.com>
2024-04-18 20:55:41 -0700

    Update neuron_data.py

--- neuron_data.py
+++ neuron_data.py
@@ -1,68 +1,118 @@
-
-import numpy as np
+import pygame
+import random
+import heapq
+import math
+import time
 import pandas as pd
 
-def get_neurons(neuron_number):
-    location = ["temporal_lobe", "frontal_lobe", "occipital_lobe", "parietal_lobe"]
-    neurons = []
-    for x in range(0,neuron_number):
-        
-        specific_location = np.random.choice(location) # WHICH QUANDRANT OF THE SCREEN WILL THE NEURON BE TOP RIGHT.. TOP LEFT ETC.
+# Define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+PINK=(255,192,203)
+RED=(255,0,0)
 
-        connection_bias = np.random.rand()
-        error = np.random.uniform(0, 0.2)  # ERROR VARIABLE NEEDED FOR WEIGHT CALCULATION BETWEEN TWO NEURONS
-        index = x  # UNIQUE INDEX FOR EACH NEURON, THIS IS HOW WE INTENTIFY UNIQUE VERTICES
 
-        neuron_size = np.random.randint(1,4) # INDICATES HOW BIGTHE NEURON WILL BE ON THE SCREEN FROM 1-3 SIZES 
+BACK_IMAGE= pygame.image.load("brain2.png")
 
-        connection_num = np.random.randint(1, 50) # LISTS THE NUMBER OF CONNECTION EACH NEURON HAS
-        
-        # Generate random connections for each neuron, excluding self-loop
-        connections = np.random.choice(np.delete(np.arange(neuron_number), x), #DELETES DUPLICATES AND GENERATES A LIST OF CONNECTIONS
-                                        size=connection_num, replace=False).tolist() # GETS LIST OF SPECIFIC CONNECTIONS 
-        
-        neurons.append(({"index":index, "location": specific_location, 
-                                         "connection_bias": connection_bias,
-                                         "error": error, "neuron_size" : neuron_size, "connections": connections})) #GET ENTIRE 1-100,000 LIST OF NEURONS
-      
-    return neurons
-        
 
-# ALL FUNCTIONS WILOL BE WRITTEN WITH INDEX OF NEURON AS PARAMATERS NOT THE ACTUAL NEURON OBJECT FOR SIMPLICITY
-#THIS FUNCTION TAKES IN FROM INDEX AND TO INDEX FOR NEURONS AND CALCULATES WEIGHT, METHOD OF WEIGHT CALCULATION DONE IS IRRELEVANT BUT BIOLOGICALLY ACCURATE
-def get_weight(from_neuron, to_neuron, neurons): 
-    weight = 0
-    weight+=(1-from_neuron["connection_bias"])
-    weight += from_neuron["error"]
+
+
+def row_to_neuron(row):
+    return {
+        "index": row["index"],
+        "location": row["location"],
+        "connection_bias": row["connection_bias"],
+        "error": row["error"],
+        "neuron_size": row["neuron_size"],
+        "connections": row["connections"]
+    }
+class Neuron:
+    def __init__(self, neuron_dict):
+        self.index = neuron_dict["index"]
+        self.location= neuron_dict["location"]
+        self.connection_bias = neuron_dict["connection_bias"]
+        self.error=neuron_dict["error"]
+        self.neuron_size=neuron_dict["neuron_size"]
+        self.connections = []
+        self.x_coord = 0
+        self.y_coord = 0
+        self.color = YELLOW
+        self.selected=False
+
+    def set_x(self, x):
+        self.x_coord = x
     
-    location = ["temporal_lobe", "frontal_lobe", "occipital_lobe", "parietal_lobe"]
-    from_location=from_neuron["location"]
-    to_location=to_neuron["location"]
+    def set_y(self,y):
+        self.y_coord=y
+    
+    def connection_add(self, neuron_index):
+        self.connections.append(neuron_index)
 
-    num1,num2=0,0
 
-    for x in range(4):
-        if location[x]==from_location:
-            num1=x
-        if location[x]==to_location:
-            num2=x
-    weight += (abs(num2-num1))
-    weight*=10
-    weight = round(weight)
-    return weight
 
-import heapq
-def heuristic_cost(neuron1, neuron2, neurons):
-    # Calculate the Euclidean distance between neurons as heuristic cost
-    return get_weight(neuron1,neuron2,neurons)
+def generate_subset_neurons(subset_number):
+    neuron_dataframe = pd.read_json('data.json')
+    neurons = [row_to_neuron(row) for _, row in neuron_dataframe.iterrows()]
+    print(len(neurons))
+    in_graph=[]
+    in_graph = set(in_graph)
+    first_neuron = neurons[random.randint(0,100000)]
+    in_graph.add(first_neuron["index"])
 
-def get_neighbors(neuron, neurons):
-    # Retrieve neighboring neurons based on connections
-    return [neurons[i] for i in neuron["connections"]]
+    current_Subset_lenght = 1
+    random.seed(422) #random seed
 
-def astar(start_index, goal_index, neurons):
-    start_neuron = neurons[start_index]
-    goal_neuron = neurons[goal_index]
+    while(current_Subset_lenght!=subset_number):
+        random_neuron = neurons[random.randint(0,99999)]
+        if random_neuron["index"] not in in_graph:
+            in_graph.add(random_neuron["index"])
+            current_Subset_lenght+=1
+
+    print("debug")
+    neuron_list={}
+    
+    in_graph = list(in_graph)
+    for neuron in in_graph:
+        num_edges = random.randint(1,2)
+        x=0
+        new_neuron = Neuron(neurons[neuron])
+        while(x!=num_edges):
+            random_neuron = in_graph[random.randint(0,len(in_graph)-1)]
+            if(random_neuron!=neuron):
+                x+=1
+                
+                new_neuron.connection_add(random_neuron)
+        neuron_list[new_neuron.index] = new_neuron
+
+    return neuron_list
+
+
+def find_closest_neighbors(nodes):
+    for node in nodes.values():
+        # Calculate distances to all other nodes
+        distances = []
+        for other_node in nodes.values():
+            if other_node != node:
+                distance = math.sqrt((other_node.x_coord - node.x_coord) ** 2 + (other_node.y_coord - node.y_coord) ** 2)
+                distances.append((other_node.index, distance))
+        
+        # Sort distances by distance
+        distances.sort(key=lambda x: x[1])
+        
+        # Add the two closest neighbors to the connections attribute
+        for neighbor, _ in distances[:2]:
+            node.connections.append(neighbor)
+def get_weight(a,b):
+    return 100*((1-a.connection_bias)*(1-b.connection_bias))
+
+def astar(start_index, goal_index, neurons, screen):
+
+    
 
     # Priority queue to store open nodes
     open_nodes = [(0, start_index)]
@@ -74,59 +124,192 @@ def astar(start_index, goal_index, neurons):
 
     while open_nodes:
         _, current_neuron = heapq.heappop(open_nodes)
+        screen.fill((230, 230, 255))
+        screen.blit(BACK_IMAGE,(0,0))
+        for neuron in neurons.values():
+            
+            if neuron.selected==False:
+                if(neuron.location=="occipital_lobe"):
+                    pygame.draw.circle(screen, YELLOW, (neuron.x_coord,neuron.y_coord), neuron.neuron_size*2)
+                if(neuron.location=="temporal_lobe"):
+                    pygame.draw.circle(screen, GREEN, (neuron.x_coord,neuron.y_coord), neuron.neuron_size*2)
+                if(neuron.location=="parietal_lobe"):
+                    pygame.draw.circle(screen, PINK, (neuron.x_coord,neuron.y_coord), neuron.neuron_size*2)
+                if(neuron.location=="frontal_lobe"):
+                    pygame.draw.circle(screen, RED, (neuron.x_coord,neuron.y_coord), neuron.neuron_size*2)
+            else:
+                pygame.draw.circle(screen, GRAY, (neuron.x_coord,neuron.y_coord), neuron.neuron_size*2)
+        pygame.draw.circle(screen, GRAY, (neurons[current_neuron].x_coord,neurons[current_neuron].y_coord), neur)
 
         if current_neuron == goal_index:
             # Reconstruct path
             path = []
+            
+            last_neuron=0
             while current_neuron in parent:
+                print("happen")
                 path.append(current_neuron)
+                pygame.draw.circle(screen, GRAY, (neurons[current_neuron].x_coord,neurons[current_neuron].y_coord), 10)
+                pygame.draw.line(screen, WHITE, (neurons[current_neuron].x_coord,neurons[current_neuron].y_coord), (neurons[parent[current_neuron]].x_coord, neurons[parent[current_neuron]].y_coord), 5)
+                pygame.display.flip()
+                last_neuron=current_neuron
                 current_neuron = parent[current_neuron]
+                time.sleep(1)
+            pygame.draw.line(screen, GREEN, (neurons[start_index].x_coord,neurons[start_index].y_coord), (neurons[path[::-1][0]].x_coord, neurons[path[::-1][0]].y_coord), 5)
+            time.sleep(5)
+            screen.fill((230, 230, 255))
+            screen.blit(BACK_IMAGE,(0,0))
             path.append(start_index)
+            print(path[::-1])
             return path[::-1]
 
-        for neighbor in get_neighbors(neurons[current_neuron], neurons):
+        for neighbor in neurons[current_neuron].connections:
+            pygame.draw.line(screen, GRAY, (neurons[current_neuron].x_coord,neurons[current_neuron].y_coord), (neurons[neighbor].x_coord,neurons[neighbor].y_coord), 1)
             tentative_g_score = visited[current_neuron] + 1
-            if tentative_g_score < visited.get(neighbor["index"], float('inf')):
-                parent[neighbor["index"]] = current_neuron
-                visited[neighbor["index"]] = tentative_g_score
-                f_score = tentative_g_score + heuristic_cost(neighbor, goal_neuron,neurons)
-                heapq.heappush(open_nodes, (f_score, neighbor["index"]))
+            if tentative_g_score < visited.get(neighbor, float('inf')):
+                parent[neighbor] = current_neuron
+                visited[neighbor] = tentative_g_score
+                f_score = tentative_g_score + get_weight(neurons[neighbor], neurons[goal_index])
+                heapq.heappush(open_nodes, (f_score, neighbor)) 
+        time.sleep(0.05)
+        pygame.display.flip()
 
-    return None  # No path found
+def main2():
+    neurons = generate_subset_neurons(50)
+    pos_dict={}
 
-def row_to_neuron(row):
-    return {
-        "index": row["index"],
-        "location": row["location"],
-        "connection_bias": row["connection_bias"],
-        "error": row["error"],
-        "neuron_size": row["neuron_size"],
-        "connections": row["connections"]
-    }
+    
 
-# Convert DataFrame to a list of neurons
+    
+
+    for neuron in neurons.values():
+        if(neuron.location=="occipital_lobe"):
+            xmin=70
+            xmax=500
+            ymin=500
+            ymax=734
+
+            x_loc = random.randint(xmin,xmax)
+            y_loc = random.randint(ymin, math.floor(600+(134/600)*x_loc))
+            pos_dict[neuron.index] = (x_loc,y_loc)
+            neuron.set_x(x_loc)
+            neuron.set_y(y_loc)
+        if(neuron.location=="temporal_lobe"):
+            xmin=70
+            xmax=500
+            ymin=200
+            ymax=500
+            
+            x_loc = random.randint(xmin,xmax)
+            
+            y_loc= random.randint(math.floor(450+(-250/430)*x_loc),ymax)
+            pos_dict[neuron.index] = (x_loc,y_loc)
+
+            neuron.set_x(x_loc)
+            neuron.set_y(y_loc) 
+        if(neuron.location=="parietal_lobe"):
+            xmin=500
+            xmax=950
+            ymin=500
+            ymax=700
+            
+            x_loc = random.randint(xmin,xmax)
+            
+            y_loc= random.randint(ymin,math.floor(935+(-134/400)*x_loc))
+            
+            pos_dict[neuron.index] = (x_loc,y_loc)
+            neuron.set_x(x_loc)
+            neuron.set_y(y_loc)
+        if(neuron.location=="frontal_lobe"):
+            xmin=500
+            xmax=950
+            ymin=250
+            ymax=500
+            
+            x_loc = random.randint(xmin,xmax)
+            
+            y_loc= random.randint(math.floor(-83.3+(1/2)*x_loc),ymax)
+            
+            pos_dict[neuron.index] = (x_loc,y_loc)
+            neuron.set_x(x_loc)
+            neuron.set_y(y_loc)
+
+    find_closest_neighbors(neurons)
+
+    for neuron in neurons.values():
+        pos_dict[neuron.index] = (neuron.x_coord,neuron.y_coord)
+    
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((1000, 834 ))
+    pygame.display.set_caption("Brain Interface")
+    
+    running = True
+    start_index=None
+    goal_index=None
+    
+    while running:
+        screen.fill((230, 230, 255))
+        screen.blit(BACK_IMAGE,(0,0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                
+                mouse_pos = pygame.mouse.get_pos()
+                for neuron in neurons.values():
+                        distance = ((neuron.x_coord - mouse_pos[0]) ** 2 + (neuron.y_coord - mouse_pos[1]) ** 2) ** 0.5
+                        if distance <= neuron.neuron_size*2:  
+                            if neuron.selected: neuron.selected=False
+                            else: neuron.selected=True
+                            if start_index is None:
+                                start_index = neuron.index
+                                print(start_index)
+                            elif goal_index is None:
+                                goal_index = neuron.index
+                                print(goal_index)
+                            break
+        path=[]  
+        if start_index!=None and goal_index!= None:
+            path=astar(start_index,goal_index,neurons,screen)
+            time.sleep(2)
+            print("hm")
+            break
+            
+        for neuron in neurons.values():
+            
+            if neuron.selected==False:
+                if(neuron.location=="occipital_lobe"):
+                    pygame.draw.circle(screen, YELLOW, (neuron.x_coord,neuron.y_coord), neuron.neuron_size*2)
+                if(neuron.location=="temporal_lobe"):
+                    pygame.draw.circle(screen, GREEN, (neuron.x_coord,neuron.y_coord), neuron.neuron_size*2)
+                if(neuron.location=="parietal_lobe"):
+                    pygame.draw.circle(screen, PINK, (neuron.x_coord,neuron.y_coord), neuron.neuron_size*2)
+                if(neuron.location=="frontal_lobe"):
+                    pygame.draw.circle(screen, RED, (neuron.x_coord,neuron.y_coord), neuron.neuron_size*2)
+                if len(path)!=0:
+                    x=0
+                    while(x<len(path)-1):
+                        pygame.draw.circle(screen, GRAY, pos_dict[path[x]],  neurons[path[x]].neuron_size)
+                        pygame.draw.circle(screen, GRAY, pos_dict[path[x+1]],  neurons[path[x+1]].neuron_size)
+                        pygame.draw.line(screen, WHITE, pos_dict[path[x]], pos_dict[path[x+1]], 1)
+                        x+=1
+            else:
+                pygame.draw.circle(screen, GRAY, (neuron.x_coord,neuron.y_coord), neuron.neuron_size*2)
+
+        
+        
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
+    
 
 
 
 
-def main():
-    neuron_dataframe = pd.read_json('data.json') # THIS IS HOW YOU GET ALL THE DATA IN YOUR PYTHON PROGRAM
 
-    neurons = [row_to_neuron(row) for _, row in neuron_dataframe.iterrows()]
-
-# Print the first few neurons
-    for neuron in neurons[:5]:
-        print(neuron)
-
-    print(astar(0,10009,neurons)) # finds path between 0 and 10009, you can try yourself for any values
-    # we need to do this for 1000 neurons
+# Define node class
 
 
-# Display the DataFrame
-   
-  
-
-
-
-if __name__ == "__main__":
-    main()
+main2()
